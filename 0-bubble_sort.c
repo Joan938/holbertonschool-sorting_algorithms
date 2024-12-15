e40e33b9b28d8a16e59e76a4726590516861d20e
Joan938 <martinezibarrondojoan@gmail.com>
2024-12-14 22:04:30 -0400

    Making all the tasks of Sorting algorithms & Big O

---
+++ 0-bubble_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorts an array of integers in ascending order with Bubble sort
+ * @array: Pointer to the first element of the array
+ * @size: The size of the array
+ *
+ * Description: This function sorts the array using the Bubble sort algorithm
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	int temp;
+	int swapped;
+	size_t i, j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = 0;
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				swapped = 1;
+				print_array(array, size);
+			}
+		}
+		if (!swapped)
+		break;
+	}
+}
